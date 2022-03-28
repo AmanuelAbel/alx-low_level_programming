39cb2c327d86ce9ad4a5c528b0503d27ae498308
AmanuelAbel <amanabel2015@gmail.com>
2022-03-28 07:05:30 -0700

    new

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,16 @@
  * Return: dest
  */
 
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-for (unsigned int i = 0; i < n; i++)
-{
-	dest[i] = src[i];
-	n--;
-}
-return (dest);
+	unsigned int i;
 
+	for (i = 0; n > 0 ; i++, n--)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
+
