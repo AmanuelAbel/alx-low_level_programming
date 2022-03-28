f329e869ce36e4802dcd60d304d74323be33a03e
AmanuelAbel <amanabel2015@gmail.com>
2022-03-28 06:46:45 -0700

    my first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-char _memset(char *s, char b, unsigned int n )
+char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-       for (unsigned int i=0; i < n; i+++)
-        {
-	s[i] = b;
-	i++;
-	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
 	}
-
-    return (s);
+	return (s);
 }
