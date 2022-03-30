716ed49664caafc88d006ae541ede7f4162c5a85
AmanuelAbel <amanabel2015@gmail.com>
2022-03-30 07:11:23 -0700

    factorial using the concept of recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,15 +7,13 @@
  *
  * Return: factorial of @n
 */
-
- int factorial(int n)
+int factorial(int n)
 {
-	if (n < 0)
-	{ return -1
-	}
-	if(n <= 1)
-	{
-	return 1;
-	}
-	else return (n * factoria;l(n-1));
+	if (n < 0)		/*error -1 if number is negative*/
+		return (-1);
+	else if (n == 0)	/*return factorial of 0 = 1*/
+		return (1);
+	else			/*do factorial of n if its not less or equal to 0*/
+		return (n * factorial(n - 1));
 }
+
