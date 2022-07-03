3a6cb34201627c3c32a04582374bd49767e3b8ae
AmanuelAbel <amanabel2015@gmail.com>
2022-07-04 04:28:23 +1400

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	if (!size)
+	{
+		return (NULL);
+	}
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+	{
+		return (NULL);
+	}
+
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	return (table);
+}
